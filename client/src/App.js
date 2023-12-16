0230b70614d125d9bb264c269ce99e7b07ecd513
sarthak ganure <sarthakganure@sarthaks-MacBook-Air.local>
2023-12-16 18:53:33 +0530

    socket connection done styling left

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
-import { Route,Routes } from 'react-router-dom';
-import './App.css';
-import LobbyScreen from './pages/Lobby';
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import LobbyScreen from "./screens/Lobby";
+import RoomPage from "./screens/Room";
 
 function App() {
   return (
-    <>
-    <Routes>
-      <Route path='/' element={<LobbyScreen/>}/>
-    </Routes>
-    </>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<LobbyScreen />} />
+        <Route path="/room/:roomId" element={<RoomPage />} />
+      </Routes>
+    </div>
   );
 }
 
